eeac5bb183fcd87f248a7440a918a6fb1b5d4703
Kwaay <benoit.dum74@gmail.com>
2024-06-18 17:23:45 +0200

    🚀 Deploy | Portfolio V1

---
+++ components/WorkHistoryComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    v-for="{ startDate, endDate, title, location, details } in history"
+    :key="title"
+    class="work-experience"
+  >
+    <div class="experience-date">
+      <span>{{ startDate }} - {{ endDate }}</span>
+    </div>
+    <div class="experience-details">
+      <h1>{{ title }}</h1>
+      <h2>{{ location }}</h2>
+      <p>{{ details }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import history from '~/data/history';
+</script>
+
+<style scoped>
+.work-experience {
+  display: flex;
+  color: white;
+  gap: 1em;
+  padding-bottom: 2em;
+}
+
+.experience-date {
+  flex: 1.2 1;
+}
+
+.experience-details {
+  flex: 3 1;
+}
+
+.experience-details h1 {
+  margin-top: 0;
+}
+
+.experience-details h2 {
+  font-size: 1em;
+  font-weight: 300;
+  color: #466fff;
+}
+
+.experience-date span {
+  background-color: #272f52;
+  padding: 0.5em;
+  border-radius: 10px;
+  display: block;
+  text-align: center;
+}
+
+@media screen and (max-width: 500px) {
+  .work-experience {
+    flex-direction: column;
+  }
+}
+</style>
---
+++ data/history.js
@@ -0,0 +1,28 @@
+const history = [
+  {
+    title: 'Développeur Web',
+    location: 'Toulouse, Occitanie, France (100% Télétravail)',
+    details:
+      "Mise en place d'un outil de création de recettes avec des informations d'impacts environnementaux",
+    startDate: '08/2022',
+    endDate: "aujourd'hui",
+  },
+  {
+    title: 'Développeur Web',
+    location: 'Side Project',
+    details:
+      "Création d'un script de scraping pour récupèrer les détails d'offres d'emploi des différents sites, création d'un moteur de recherche pour afficher les résultats en fonction des langages cochés",
+    startDate: '03/2022',
+    endDate: "aujourd'hui",
+  },
+  {
+    title: 'Développeur Web',
+    location: 'Projet fictif',
+    details:
+      "Création d'un API REST + Front d'un prétendu réseau social, dashboard pour modifier toutes les données",
+    startDate: '03/2022',
+    endDate: '12/2021',
+  },
+];
+
+export default history;
